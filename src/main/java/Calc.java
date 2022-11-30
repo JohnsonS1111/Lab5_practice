75f973be2cf5f0590e80d2bb28a40cb3cb80e08f
Johnson <Zqsn2V!9>
2022-11-30 13:21:48 +0000

    new java class Calc added new test class CalcTest added

---
+++ src/main/java/Calc.java
@@ -0,0 +1,11 @@
+public class Calc {
+    private int num1;
+    private int num2;
+    private int ans;
+
+    public Calc(){
+        num1 = 0;
+        num2 = 0;
+        ans = 0;
+    }
+}
---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,15 @@
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class CalcTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown(){
+    }
+}
